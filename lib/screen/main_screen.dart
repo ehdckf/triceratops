eb38eea10cb355087dbae7e5d94c95b221521194
ehdckf <ehdcjf@funnyfig.com>
2023-11-07 21:26:18 +0900

    :sparkles: shared_preferences

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:shared_preferences/shared_preferences.dart";
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,6 +10,13 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   TextEditingController introduceController = TextEditingController();
+
+  bool isEditMode = false;
+  @override
+  void initState() {
+    super.initState();
+    getIntroduceData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +41,7 @@ class _MainScreenState extends State<MainScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                  margin: EdgeInsets.all(16),
+                  margin: const EdgeInsets.all(16),
                   width: 370,
                   height: 400,
                   child: Image.asset(
@@ -41,115 +49,157 @@ class _MainScreenState extends State<MainScreen> {
                     // fit: BoxFit.cover,
                   )),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Row(
                   children: [
                     Container(
                       width: 150,
-                      child: Text(
+                      child: const Text(
                         '학명',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Text('Triceratops'),
+                    const Text('Triceratops'),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Row(
                   children: [
                     Container(
                       width: 150,
-                      child: Text(
+                      child: const Text(
                         '생존시기',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Text('백악기 후기'),
+                    const Text('백악기 후기'),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Row(
                   children: [
                     Container(
                       width: 150,
-                      child: Text(
+                      child: const Text(
                         '발견되는 지역',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Text('북아메리카'),
+                    const Text('북아메리카'),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Row(
                   children: [
                     Container(
                       width: 150,
-                      child: Text(
+                      child: const Text(
                         '발견되는 지층',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Text('헬 크릭 지층'),
+                    const Text('헬 크릭 지층'),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Row(
                   children: [
                     Container(
                       width: 150,
-                      child: Text(
+                      child: const Text(
                         '몸길이',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Text('8~9m'),
+                    const Text('8~9m'),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                 child: Row(
                   children: [
                     Container(
                       width: 150,
-                      child: Text(
+                      child: const Text(
                         '몸무게',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    Text('5~9t'),
+                    const Text('5~9t'),
                   ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(left: 16, top: 16),
-                child: Text(
-                  '자기소개',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 16, top: 16),
+                    child: const Text(
+                      '자기소개',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      if (isEditMode == true) {
+                        if (introduceController.text.isEmpty) {
+                          var snackBar = SnackBar(
+                            content: Text('자기소개 입력값이 비어있당..'),
+                            duration: Duration(seconds: 2),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          return;
+                        }
+
+                        var sharedPref = await SharedPreferences.getInstance();
+                        sharedPref.setString(
+                            'introduce', introduceController.text);
+                      }
+                      setState(() {
+                        isEditMode = !isEditMode;
+                      });
+                    },
+                    child: Container(
+                        margin: const EdgeInsets.only(right: 16, top: 16),
+                        child: Icon(Icons.mode_edit,
+                            color: isEditMode
+                                ? Colors.green.shade700
+                                : Colors.black,
+                            size: 24)),
+                  )
+                ],
               ),
               Container(
-                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 child: TextField(
                   controller: introduceController,
                   maxLines: 5,
+                  enabled: isEditMode,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.brown))),
+                          borderSide: const BorderSide(color: Colors.brown))),
                 ),
               )
             ],
           ),
         ));
+  }
+
+  Future<void> getIntroduceData() async {
+    var sharedPref = await SharedPreferences.getInstance();
+    String introduceMessage =
+        sharedPref.getString('introduce')?.toString() ?? '';
+    introduceController.text = introduceMessage;
   }
 }
